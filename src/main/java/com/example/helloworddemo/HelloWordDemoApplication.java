a4e6897cc618bad48cb4eaed1437b12cbad43fc5
gao_xianglong <gao_xianglong@sina.com>
2020-08-03 11:45:08 +0800

    提交

--- src/main/java/com/example/helloworddemo/HelloWordDemoApplication.java
+++ src/main/java/com/example/helloworddemo/HelloWordDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWordDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWordDemoApplication.class, args);
+        System.out.println("小亮亮");
     }
 
 }
