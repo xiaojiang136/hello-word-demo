cb267609e9518486678c079690933d717e3143b2
eleven136 <1587780056@qq.com>
2020-08-03 11:22:46 +0800

    Initial commit

---
+++ src/main/java/com/example/helloworddemo/HelloWordDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworddemo/HelloWordDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
